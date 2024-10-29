4611c80ce31b22c4c688083e949e57a0a2141041
tree <tree@hidden.cz>
2024-10-29 12:54:43 +0100

    events redesign

--- src/lib/components/EventList.svelte
+++ src/lib/components/EventList.svelte
@@ -11,14 +11,21 @@
 <div class="grid grid-cols-1 gap-4 mt-4">
     {#each events as ev}
         <div>
-            <div class="flex flex-wrap {ev.major ? 'text-2xl' : 'text-2xl'}">
-                <div class="mr-4 opacity-50">
-                    {#if ev.place?.country}
-                        <span class="uppercase">{ev.place?.country}</span>/{ev
-                            .place?.city || "YY"}
-                    {:else}
-                        Online
-                    {/if}
+            <div
+                class="md:flex items-center {ev.major
+                    ? 'text-2xl'
+                    : 'text-2xl'}"
+            >
+                <div
+                    class="mt-1 text-nowrap inline-block opacity-75 mr-4 text-xl font-mono {ev.major
+                        ? 'font-semibold'
+                        : ''}"
+                >
+                    {new Date(ev.date).toISOString().slice(0, 10)}
+                    <!--{#if ev.days && ev.days > 1}
+            - {addDays(new Date(ev.date), ev.days - 1)
+                .toISOString()
+                .slice(0, 10)}{/if}-->
                 </div>
                 <div class="grow">
                     <h3 class="inline">
@@ -27,6 +34,9 @@
                             alt={ev.name}
                             class={ev.major ? "font-semibold" : ""}>{ev.name}</a
                         >
+                        {#if ev.days && ev.days > 1}
+                            <span class="opacity-50">({ev.days}d)</span>
+                        {/if}
                     </h3>
                     {#if person}
                         {#if ev.organizers?.includes(person)}
@@ -47,13 +57,13 @@
                         {/if}
                     {/if}
                 </div>
-                <div class="mt-1 inline-block text-xl opacity-50">
-                    {new Date(ev.date).toLocaleDateString(lang)}
-                    {#if ev.days && ev.days > 1}
-                        - {addDays(
-                            new Date(ev.date),
-                            ev.days - 1,
-                        ).toLocaleDateString(lang)}{/if}
+                <div class="mt-2 sm:mt-0 mr-4 opacity-50 text-xl">
+                    {#if ev.place?.country}
+                        <span class="uppercase">{ev.place?.country}</span>/{ev
+                            .place?.city || "YY"}
+                    {:else}
+                        Online
+                    {/if}
                 </div>
             </div>
         </div>
