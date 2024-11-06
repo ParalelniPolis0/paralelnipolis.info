16f59c6d8622a637d693ad8c2b3ff738331a345f
tree <tree@hidden.cz>
2024-11-06 08:01:30 +0100

    use media-chrome for video

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,6 +1,7 @@
 <script>
     import { imgHashUrl, archiveStorageUrl } from "$lib/utils.js";
     import Youtube from "$lib/components/Youtube.svelte";
+    import "media-chrome";
 
     const { item } = $props();
     let storage = $derived(item.storage);
@@ -8,16 +9,46 @@
 </script>
 
 {#if storage}
-    <video
+    <!--video
+            width="100%"
+            height="100%"
+            class="aspect-video"
+            controls
+            poster={}
+        >
+            <source
+                src={archiveStorageUrl(storage.source.path)}
+                type="video/mp4"
+            />
+            Your browser does not support the video tag.
+        </video-->
+    <media-controller
         width="100%"
         height="100%"
         class="aspect-video"
-        controls
-        poster={imgHashUrl("archive", item.imgHash, "l")}
+        role="region"
+        aria-label="video player"
     >
-        <source src={archiveStorageUrl(storage.source.path)} type="video/mp4" />
-        Your browser does not support the video tag.
-    </video>
+        <video
+            slot="media"
+            src={archiveStorageUrl(storage.source.path)}
+            poster={imgHashUrl("archive", item.imgHash, "l")}
+        >
+        </video>
+        <media-control-bar>
+            <media-play-button></media-play-button>
+            <media-seek-forward-button></media-seek-forward-button>
+            <media-seek-backward-button></media-seek-backward-button>
+            <media-mute-button></media-mute-button>
+            <media-time-range></media-time-range>
+            <media-time-display></media-time-display>
+            <!--media-captions-button></media-captions-button-->
+            <media-fullscreen-button></media-fullscreen-button>
+            <media-playback-rate-button></media-playback-rate-button>
+            <media-pip-button></media-pip-button>
+            <media-fullscreen-button></media-fullscreen-button>
+        </media-control-bar>
+    </media-controller>
 {:else}
     <Youtube id={youtubeId} autoplay="true" />
 {/if}
