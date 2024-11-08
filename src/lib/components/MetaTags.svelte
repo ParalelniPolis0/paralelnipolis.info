bf30117fb9a84d3a1e9085a42a506a0d416b86b4
tree <tree@hidden.cz>
2024-11-08 09:24:02 +0100

    fix trim

--- src/lib/components/MetaTags.svelte
+++ src/lib/components/MetaTags.svelte
@@ -1,14 +1,14 @@
 <script>
     import { config } from "$lib/data.js";
     let { title, description, url, img } = $props();
-    title = title.trim();
+    let trimmedTitle = $derived(title.trim());
 </script>
 
-<title>{title} | {config.title}</title>
-<meta name="title" content={title} />
+<title>{trimmedTitle} | {config.title}</title>
+<meta name="title" content={trimmedTitle} />
 <meta name="description" content={description} />
 
-<meta property="og:title" content={title} />
+<meta property="og:title" content={trimmedTitle} />
 <meta property="og:description" content={description} />
 
 <meta property="og:type" content="website" />
