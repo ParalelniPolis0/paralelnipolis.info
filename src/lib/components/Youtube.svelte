5f258cfd865cc59a7e5390b35796c37a9084a1a2
tree <tree@tree.fail>
2025-02-20 03:31:54 +0100

    change links for forum

--- src/lib/components/Youtube.svelte
+++ src/lib/components/Youtube.svelte
@@ -8,6 +8,6 @@
     width="100%"
     height="100%"
     class="aspect-video bg-gray-200 dark:bg-gray-800 rounded"
-    src="https://www.youtube.com/embed/{id}?autoplay=0&enablejsapi=1&playsinline=1&fs=1&origin=https://paralelnipolis.cz"
+    src="https://www.youtube.com/embed/{id}?autoplay=0&enablejsapi=1&playsinline=1&fs=1&origin=https://parallelpolis.info"
     frameborder="0"
 ></iframe>
